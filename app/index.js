80c7e5d18a080f30af85208eda2e83a7c8cc508f
carson.wang <carson.wang@droi.com>
2018-03-21 11:38:50 +0800

    test event handle

--- app/index.js
+++ app/index.js
@@ -7,28 +7,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Timer extends React.Component {
+const TodoList = (props) => (
+	<ul>
+		{
+			props.items.map((item) => (
+				<li key={item.id}>{item.text}</li>
+			))
+		}
+	</ul>
+)
+
+class TodoApp extends React.Component {
 	constructor(props) {
 		super(props);
-        this.tick = this.tick.bind(this);
+		this.onChange = this.onChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 		this.state = {
-			secondsElapsed: 0,
+			items: [],
+			text: '',
 		}
 	}
-	tick() {
-	    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+	onChange(e) {
+    	this.setState({text: e.target.value});
 	}
-	componentDidMount() {
-	    this.interval = setInterval(this.tick, 1000);
-	}
-	componentWillUnmount() {
-		clearInterval(this.interval);
+	handleSubmit(e) {
+    	e.preventDefault();
+    	const nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
+    	const nextText = '';
+    	this.setState({items: nextItems, text: nextText});
 	}
 	render() {
 	    return (
-	      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
+	      <div>
+	        <h3>TODO</h3>
+	        <TodoList items={this.state.items} />
+	        <form onSubmit={this.handleSubmit}>
+	          <input onChange={this.onChange} value={this.state.text} />
+	          <button>{'Add #' + (this.state.items.length + 1)}</button>
+	        </form>
+	      </div>
 	    );
 	}
 }
 
-ReactDOM.render(<Timer />, document.getElementById('app'));
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
