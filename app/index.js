3b829a26fd45fa882e659be73eeb4ea6954ec840
carson.wang <carson.wang@droi.com>
2018-03-21 11:32:03 +0800

    test stateful Component

--- app/index.js
+++ app/index.js
@@ -7,19 +7,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hello, World!</h1>
-            </div>
-        );
-    }
+class Timer extends React.Component {
+	constructor(props) {
+		super(props);
+        this.tick = this.tick.bind(this);
+		this.state = {
+			secondsElapsed: 0,
+		}
+	}
+	tick() {
+	    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+	}
+	componentDidMount() {
+	    this.interval = setInterval(this.tick, 1000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+	render() {
+	    return (
+	      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
+	    );
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Timer />, document.getElementById('app'));
